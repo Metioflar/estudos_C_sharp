303e2d868e6c5c51b575694691e4e53a2850794c
Nico <112582889+Metioflar@users.noreply.github.com>
2023-08-09 18:52:51 -0300

    Create RevisaoLogica.cs

---
+++ RevisaoLogica.cs
@@ -0,0 +1,51 @@
+
+namespace RevisaoLogica
+{
+using static System.Console;
+
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //WriteLine("Hello, World!");
+            //Write("Olá, mundo");
+            //var qualquerCoisa = 1;
+            // string palavra;
+            // string outrPalavra = "olá";
+            // int numero = 10;
+            // bool completa = false;
+            // char letra = 'a';
+            // char uniCode = '\u0041';
+            //  byte numeroPequeno = 123;
+            //  int numeroMedio = 231923123;
+            //  long numeroGrandao = 3123123123123123123;
+            //  float numeroEnorme = 230128312234565121311232212392.13423128312f;
+            //  double numeroEnormeDois = 123123123123123123124124122123123124124122123123124124122123123124124122123123124124122.1231231231231231231231231241241221;
+            //  string texto = "olhai maluco muito top";
+            //  Object obj = "nome";
+            // Object obj2 = 4.5f;
+            // int[] numeros = new int[10];//nesse caso só tem os espaços delimitados mas sem valores
+            // int[] numeros2 = new int[3] { 1, 2, 3 };//forma de inicializar um array já com os valores
+            // int[,] matriz = new int[3,3]; //pra fazer uma matriz de duas dimenções
+            // matriz[1, 1] = 3; //colocando o valor 3 na posição 1 e 1.
+            // ReadKey();
+            //indices começam de 0 assim como no python.
+            //regras de boas praticas: não pode usar digitos na primeira letra do nome
+            // não pode colocar espaço ou caracteres especiais que não seja _
+            //nomes que façam sentido
+            //camel case
+            //  double n7 = (3.2 + 4.2) * 2;
+
+            //  WriteLine(n7);
+
+
+            // var numeroUm = 5;
+                //calcule as expressões abaixo, observando a precedência dos operadores.
+                //Escreva um programa que mostre na tela o resultado de cada expressão,
+                //um por vez, colocando a expressão e o resultado cada vez que uma tecla
+                //for pressionada.
+            
+        }
+    }
+}
