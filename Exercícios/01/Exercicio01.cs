543f3bfe32123658b5f289e0cfe2e467f6864e79
Nico <112582889+Metioflar@users.noreply.github.com>
2023-08-13 10:10:10 -0300

    Add files via upload

---
+++ Exercícios/01/Exercicio01.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira seu nome para iniciarmos! \n");
+            string nome = Console.ReadLine();
+            Console.WriteLine($"Bem-vindo, {nome}!");
+
+
+        }
+    }
+}
---
+++ Exercícios/02/Exercicio02.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira seu nome para iniciarmos! \n");
+            string nome = Console.ReadLine();
+            Console.Write("Insira sua idade: \n");
+            int idade = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Insira sua altura: \n");
+            double altura = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Insira seu peso: \n");
+            double peso = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"nome: {nome}\n idade: {idade} \n altura: {altura} \n peso: {peso}.");
+        }
+    }
+}
