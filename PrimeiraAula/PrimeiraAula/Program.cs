36c78886249869ab5a1d9b2b9812ab03adca7fbc
Nico <112582889+Metioflar@users.noreply.github.com>
2023-08-13 10:12:34 -0300

    Add files via upload

---
+++ PrimeiraAula/PrimeiraAula/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira o valor do RAIO do círculo para que possamos calcular a sua área. \n");
+            double raio = Convert.ToDouble(Console.ReadLine());
+            double area = Math.PI * Math.Pow(raio, 2);
+            Console.WriteLine($"A área do círculo é {area.ToString("N2")}");
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
---
+++ SistemaDeSenha/Program.cs
@@ -0,0 +1,101 @@
+﻿/*using System;
+namespace praticando
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = 10;
+            int b = 20;
+            if (a > b)
+            {
+                Console.WriteLine("a é maior que b");
+            }
+            else if (a < b)
+            {
+                Console.WriteLine("a é menor que b");
+            }
+            else
+            {
+                Console.WriteLine("os numeros são iguais");
+            }
+
+        }
+      
+    }
+}
+*/
+using System;
+
+class senhas
+{
+    static int senhaPreferencial = 1;
+    static int senhaComum = 1;
+
+    static void Main()
+    {
+        bool sair = false;
+
+        while (!sair)
+        {
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1. Emitir Senha Preferencial");
+            Console.WriteLine("2. Emitir Senha Comum");
+            Console.WriteLine("3. Chamar Próxima Senha");
+            Console.WriteLine("4. Sair");
+
+            Console.Write("Escolha uma opção: ");
+            int opcao = int.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1:
+                    EmitirSenhaPreferencial();
+                    break;
+                case 2:
+                    EmitirSenhaComum();
+                    break;
+                case 3:
+                    ChamarProximaSenha();
+                    break;
+                case 4:
+                    sair = true;
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida. Por favor, escolha uma opção válida.");
+                    break;
+            }
+
+            Console.WriteLine();
+        }
+        static void EmitirSenhaPreferencial()
+        {
+            string senha = "P" + senhaPreferencial.ToString("D3");
+            Console.WriteLine("Senha emitida: " + senha);
+            senhaPreferencial++;
+        }
+
+        static void EmitirSenhaComum()
+        {
+            string senha = "C" + senhaComum.ToString("D3");
+            Console.WriteLine("Senha emitida: " + senha);
+            senhaComum++;
+        }
+
+        static void ChamarProximaSenha()
+        {
+            if (senhaPreferencial <= senhaComum)
+            {
+                string senha = "P" + (senhaPreferencial - 1).ToString("D3");
+                Console.WriteLine("Chamando próxima senha: " + senha);
+            }
+            else
+            {
+                string senha = "C" + (senhaComum - 1).ToString("D3");
+                Console.WriteLine("Chamando próxima senha: " + senha);
+            }
+        }
+    }
+
+  
+}
